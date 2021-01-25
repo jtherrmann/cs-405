18f0bf5c550ced19a4137f8384f187ab85d519bb
Jake Herrmann <jtherrmann1@gmail.com>
2021-01-25 07:18:37 -0900

    Add new game button

--- hw1/tic_tac_toe/gui.py
+++ hw1/tic_tac_toe/gui.py
@@ -1,4 +1,5 @@
 import tkinter
+from typing import Optional
 
 from .game import Game
 
@@ -18,11 +19,31 @@ TIMER_MS = 50
 
 
 # ----------------------------------------------------------------------
+# Game wrapper
+# ----------------------------------------------------------------------
+
+class GameWrapper:
+    def __init__(self):
+        self.game: Optional[Game] = None
+        self._debugOn = False
+
+    def set_debug(self, debugOn):
+        self._debugOn = debugOn
+
+    def new_game(self):
+        self.game = Game(SIZE, humanX=True, humanO=True, debugOn=self._debugOn)
+        canvas.delete('all')
+        draw_grid()
+        timer()
+
+
+# ----------------------------------------------------------------------
 # Setup
 # ----------------------------------------------------------------------
 
 root = tkinter.Tk()
 canvas = tkinter.Canvas(root, bg=BG, width=CANVAS_SIZE, height=CANVAS_SIZE)
+gamewrapper = GameWrapper()
 
 
 # ----------------------------------------------------------------------
@@ -44,37 +65,30 @@ def draw_move(x, y, char):
 # Event handlers
 # ----------------------------------------------------------------------
 
-def click_handler(game):
-
-    def handler(event):
+def click_handler(event):
+    if gamewrapper.game:
         move = point_to_move(event.x, event.y)
-        game.set_human_move(move)
-
-    return handler
+        gamewrapper.game.set_human_move(move)
 
 
 # ----------------------------------------------------------------------
 # Timer loop
 # ----------------------------------------------------------------------
 
-def start_timer(game):
-
-    def timer():
-        outcome = make_move(game)
-        if outcome is None:
-            root.after(TIMER_MS, timer)
-        else:
-            print(f'Outcome: {outcome}')  # TODO: display in gui
-
-    timer()
+def timer():
+    outcome = make_move()
+    if outcome is None:
+        root.after(TIMER_MS, timer)
+    else:
+        print(f'Outcome: {outcome}')  # TODO: display in gui
 
 
-def make_move(game):
-    move, char = game.make_move()
+def make_move():
+    move, char = gamewrapper.game.make_move()
     if move is not None:
         x, y = move_to_point(move)
         draw_move(x, y, char)
-        return game.get_outcome()
+        return gamewrapper.game.get_outcome()
     return None
 
 
@@ -102,10 +116,10 @@ def center_coord(row_or_col):
 # ----------------------------------------------------------------------
 
 def main(args):
+    gamewrapper.set_debug(args.debug)
     root.title('Tic-tac-toe')
+    root.bind('<Button-1>', click_handler)
+    button = tkinter.Button(root, text='New game', command=gamewrapper.new_game)
     canvas.pack()
-    draw_grid()
-    game = Game(SIZE, humanX=True, humanO=True, debugOn=args.debug)
-    root.bind('<Button-1>', click_handler(game))
-    start_timer(game)
+    button.pack()
     root.mainloop()
