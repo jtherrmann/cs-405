14c2f7a26155a2755115a3fd17e5cc894dc75e4b
Jake Herrmann <jtherrmann1@gmail.com>
2021-02-06 14:16:40 -0900

    Refactor minimax search

--- hw1/tic_tac_toe/search.py
+++ hw1/tic_tac_toe/search.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
 from operator import lt, gt
 from time import time
-from typing import List, Any
 
 from . import core
 from .evaluate import eval_board
@@ -29,13 +27,13 @@ class Tree:
             print(f'Nodes created: {nodes_created}')
             print(f'Search time: {total} ms\n')
 
-        self._root = self._root.best_child
-        return self._root.move
+        self._root = self._root.get_best_child()
+        return self._root.get_move()
 
     def _update_root(self, board):
-        if self._root.board != board:
-            for child in self._root.children:
-                if child.board == board:
+        if self._root.get_board() != board:
+            for child in self._root.get_children():
+                if child.get_board() == board:
                     self._root = child
                     return
             # noinspection PyUnreachableCode
@@ -45,13 +43,52 @@ class Tree:
 
 
 # TODO refactor gui so do not have to store move
-@dataclass
 class Node:
-    board: int
-    move: int = None
-    val: float = None
-    children: List = ()
-    best_child: Any = None  # TODO don't store this in node?
+
+    def __init__(self, board, move=None):
+        self._board = board
+        self._move = move
+
+        self._val = core.check_outcome(self._board)
+        self._leaf = self._val is not None
+
+        self._children = ()
+
+    def is_leaf(self):
+        return self._leaf
+
+    def is_min_node(self):
+        return bool(core.turn_bit(self._board))
+
+    def get_board(self):
+        return self._board
+
+    def get_move(self):
+        return self._move
+
+    def get_val(self):
+        return self._val
+
+    def set_val(self, val):
+        self._val = val
+
+    def get_children(self):
+        return self._children
+
+    def create_children(self):
+        if self._children:
+            return 0
+        self._children = [
+            Node(board=core.add_move(index, self._board), move=index)
+            for index in core.legal_moves(self._board)
+        ]
+        return len(self._children)
+
+    def get_best_child(self):
+        for child in self._children:
+            if child.get_val() == self._val:
+                return child
+        assert False
 
 
 tree = Tree()
@@ -60,36 +97,27 @@ tree = Tree()
 def minimax(node: Node, depth=5):
     nodes_visited, nodes_created = 1, 0
 
-    node.best_child = None
-    node.val = core.check_outcome(node.board)  # TODO store outcome in node?
-
-    if node.val is not None:
+    if node.is_leaf():
         return nodes_visited, nodes_created
 
     if depth == 0:
-        node.val = eval_board(node.board)
+        node.set_val(eval_board(node.get_board()))
         return nodes_visited, nodes_created
 
-    if not node.children:
-        node.children = [
-            Node(board=core.add_move(index, node.board), move=index)
-            for index in core.legal_moves(node.board)
-        ]
-        nodes_created += len(node.children)
+    nodes_created += node.create_children()
 
-    min_node = core.turn_bit(node.board)
-    node.val, compare = (core.INF, lt) if min_node else (core.NEG_INF, gt)
+    if node.is_min_node():
+        node.set_val(core.INF)
+        compare = lt
+    else:
+        node.set_val(core.NEG_INF)
+        compare = gt
 
-    for child in node.children:
+    for child in node.get_children():
         result = minimax(child, depth - 1)
         nodes_visited += result[0]
         nodes_created += result[1]
-        if compare(child.val, node.val):
-            node.val = child.val
-            node.best_child = child
-
-    if node.best_child is None:
-        node.best_child = node.children[0]
-        assert node.val == node.best_child.val
+        if compare(child.get_val(), node.get_val()):
+            node.set_val(child.get_val())
 
     return nodes_visited, nodes_created
